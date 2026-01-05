503b8032524a5fa7d2e620799f7554aba58f2022
yasmeen <y.abuarrah1@gmail.com>
2026-01-05 20:50:53 +0200

    Add QudusDBWeb dir.

---
+++ QudusDBWeb/QudusDBWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QudusDBWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
